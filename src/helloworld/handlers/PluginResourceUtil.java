9c9cfcf2980de79681bbb52ad6d9d8db4a823f5b
Jiazliang <Jiazliang@users.noreply.github.com>
2024-01-27 16:46:37 +0800

    功能基本完成

---
+++ src/helloworld/handlers/PluginResourceUtil.java
@@ -0,0 +1,33 @@
+package helloworld.handlers;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+import helloworld.Activator;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class PluginResourceUtil {
+
+    public static File getPluginFile(String relativePath) {
+        Bundle bundle = Platform.getBundle(Activator.getDefault().getBundle().getSymbolicName());
+
+        if (bundle != null) {
+            URL fileURL = bundle.getEntry(relativePath);
+
+            if (fileURL != null) {
+                try {
+                    URL resolvedFileURL = FileLocator.toFileURL(fileURL);
+                    return new File(resolvedFileURL.getFile());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
+    }
+}
--- src/helloworld/handlers/SampleHandler.java
+++ src/helloworld/handlers/SampleHandler.java
@@ -3,60 +3,64 @@ package helloworld.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+import java.io.File;
+
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.console.ConsolePlugin;
 import org.eclipse.ui.console.IConsole;
 import org.eclipse.ui.console.IConsoleManager;
 import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class SampleHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		
-		System.out.println("HelloWorld");
-		
-		// Executable path
-		String path = "E:\\eclipse-workspace\\HelloWorld\\lib\\example_script.exe";
-		// The console used for displaying output
-	    MessageConsole pluginConsole = findConsole("Plugin");
-	    
-		// Start the background job
-	    ProcessExecutableJob job = new ProcessExecutableJob("My Excutable Job", path, pluginConsole);
-        job.schedule();
-	
 
-//		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-//		MessageDialog.openInformation(window.getShell(), "HelloWorld", "Hello, Eclipse world");
+		System.out.println("HelloWorld");
+
+		String relativePath = "lib/example_script.exe";
+		File file = PluginResourceUtil.getPluginFile(relativePath);
+
+		if (file != null && file.exists()) {
+			System.out.println("File exists: " + file.getAbsolutePath());
+
+			// Executable path
+			String path = file.getAbsolutePath();
+			// The console used for displaying output
+			MessageConsole pluginConsole = findConsole("Plugin");
+
+			// Start the background job
+			ProcessExecutableJob job = new ProcessExecutableJob("My Excutable Job", path, pluginConsole);
+			job.schedule();
+
+		} else {
+			System.out.println("File not found.");
+		}
+
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(), "HelloWorld", "Hello, Eclipse world");
 		return null;
 	}
-	
-	// 辅助方法：根据名称查找控制台
-    private static MessageConsole findConsole(String name) {
-    	// 获取所有控制台
-        IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
-        IConsole[] consoles = consoleManager.getConsoles();
-        // 查到控制台
-        for (IConsole console : consoles) {
-            if (console.getName().equals(name) && console instanceof MessageConsole) {
-            	consoleManager.showConsoleView(console);
-                return (MessageConsole) console;
-            }
-        }
-        // 如果不存在，则创建新的控制台
-        MessageConsole console = new MessageConsole(name, null);
-        consoleManager.addConsoles(new IConsole[]{console});
-        consoleManager.showConsoleView(console);
-        return console;
-    }
-    // 判断文件是否存在
-	private static boolean isFile(String name) {
-		// 将文件路径转换为 Path 对象
-		Path path = Paths.get(name);
 
-		// 使用 Files.exists() 方法判断文件是否存在
-		return Files.exists(path);
+	// 辅助方法：根据名称查找控制台
+	private static MessageConsole findConsole(String name) {
+		// 获取所有控制台
+		IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
+		IConsole[] consoles = consoleManager.getConsoles();
+		// 查到控制台
+		for (IConsole console : consoles) {
+			if (console.getName().equals(name) && console instanceof MessageConsole) {
+				consoleManager.showConsoleView(console);
+				return (MessageConsole) console;
+			}
+		}
+		// 如果不存在，则创建新的控制台
+		MessageConsole console = new MessageConsole(name, null);
+		consoleManager.addConsoles(new IConsole[] { console });
+		consoleManager.showConsoleView(console);
+		return console;
 	}
 }
